1d6af68e3703d970392791c3245c8aa45c1f9820
jero98772 <jero98772@pop-os.localdomain>
2023-10-25 16:44:13 -0500

    add 5 games tages

--- Leyenda_Arvi.py
+++ Leyenda_Arvi.py
@@ -8,5 +8,24 @@ class webpage():
   def index():
     return render_template("index.html")
       
+  @app.route("/p1")
+  def p1():
+    return render_template("maps/1point.html")
+
+  @app.route("/p2")
+  def p2():
+    return render_template("maps/2point.html")
+
+  @app.route("/p3")
+  def p3():
+    return render_template("maps/3point.html")
+
+  @app.route("/p4")
+  def p4():
+    return render_template("maps/4point.html")
+
+  @app.route("/p5")
+  def p5():
+    return render_template("maps/5point.html")
 if __name__ == "__main__":
   app.run(debug=True,host="127.0.0.1",port=5000)
