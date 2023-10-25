0ca9d72da0e1e9843bd8a916f18de45fcd5dfa73
jero98772 <jero98772@pop-os.localdomain>
2023-10-25 16:27:00 -0500

    update

--- scripts/testmap/main.py
+++ scripts/testmap/main.py
@@ -91,7 +91,7 @@ class configMap:
         return newPath
 
     def genMapMultlayer(self,fileName,layers:list):
-        view = pdk.ViewState(latitude=6.268881044071327, longitude= -75.49672372106615, pitch=40, zoom=12)
+        view = pdk.ViewState(latitude=6.268881044071327, longitude= -75.49672372106615, pitch=40, zoom=16)
         mapCompleate = pdk.Deck(layers=layers, initial_view_state=view)
         mapCompleate.to_html(fileName)
     
@@ -103,10 +103,14 @@ columns=["lng","lat"]
 for i in range(10):
     d1,d2=generate_random_coordinates()
     data.append([d1,d2])
-my_points=[[6.268881044071327, -75.49672372106615],[6.265695702561827, -75.4951161823077],[6.268923631330613, -75.49412205704012],[6.266563029351161, -75.4965374219183],[6.266710674021357, -75.49431480119169]]
+my_points=[[-75.4951161823077,6.265695702561827],[-75.49672372106615,6.268881044071327],[-75.49431480119169,6.266710674021357],[-75.4965374219183,6.266563029351161],[-75.49412205704012,6.268923631330613],[-75.49535631183988,6.267000603026892]]
+
 print(data,my_points)
 df=pd.DataFrame(my_points, columns=columns)
-print(df)
+datap1={"path":[[[-75.4951161823077,6.265695702561827],[-75.49672372106615,6.268881044071327],[-75.49431480119169,6.266710674021357],[-75.4965374219183,6.266563029351161],[-75.49412205704012,6.268923631330613],[-75.4951161823077,6.265695702561827]]]}
+print(datap1)
+dfpath1=pd.DataFrame(datap1,columns=["path"])
+print(dfpath1)
 
 a=configMap(df)
 layer = pdk.Layer(
@@ -116,7 +120,7 @@ layer = pdk.Layer(
     opacity=0.8,
     stroked=True,
     filled=True,
-    get_position=[ 'lat','lng'],
+    get_position=['lng','lat'],
     auto_highlight=True,
     get_radius=1,
     radius_min_pixels=1,
@@ -125,6 +129,20 @@ layer = pdk.Layer(
     get_fill_color=[137, 36, 250],
     get_line_color=[0, 0, 0],
     pickable=True)
+
+newPath = pdk.Layer(
+    "PathLayer",
+    dfpath1,
+    pickable=False,
+    get_color=(0,155,0),
+    width_scale=5,
+    opacity=0.8,
+    stroked=True,
+    filled=True,
+    get_path="path",
+    width_min_pixels=5,
+    get_width=2
+)
 #b=a.getnodesMap()#a.getEmptyMap()
 #c=a.newPath()
-a.genMapMultlayer("test",[layer])
+a.genMapMultlayer("points5.html",[layer,newPath])
